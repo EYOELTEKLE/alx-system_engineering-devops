48fd0f3e7e6ef22118fe43b4567fc09622c7e2c3
EYOELTEKLE <eyoeltekle10@gmail.com>
2022-09-16 09:34:22 -0700

    lint fix trial

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,7 @@
+# puppet project
 
-exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.1.0',
+
+package {'puppet-lint':
+  ensure   => '2.1.1',
+  provider => 'gem',
 }
